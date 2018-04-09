da1b6edc3167ec084473cb6fbd2efb515f0fb483
Naxo <naxolafuente@gmail.com>
2018-04-09 06:54:36 +0200

    Updated wrong hook on FinishGame

--- test/unit/specs/App.spec.js
+++ test/unit/specs/App.spec.js
@@ -8,84 +8,84 @@ import Options from '@/components/Options'
 
 Vue.use(VueRouter)
 const myRoutes = [
-    {
-        path: '/',
-        component: Options
-    },
-    {
-        path: '/game',
-        component: GameContainer
-    },
-    {
-        path: '/botgame',
-        component: GameContainerRobot
-    },
-    {
-        path: '/finishgame',
-        component: FinishGame
-    }
+  {
+    path: '/',
+    component: Options
+  },
+  {
+    path: '/game',
+    component: GameContainer
+  },
+  {
+    path: '/botgame',
+    component: GameContainerRobot
+  },
+  {
+    path: '/finishgame',
+    component: FinishGame
+  }
 ]
 
 describe('App.vue', () => {
-    it('should render correct title to root', done => {
-        const Constructor = Vue.extend(App)
-        const router = new VueRouter({
-            routes: myRoutes
-        })
-        const vm = new Constructor({ router }).$mount()
-        router.push('/')
-        Vue.nextTick(() => {
-            expect(vm.$el.querySelector('h1').textContent).to.equal('Color Game')
-            done()
-        })
+  it('should render correct title to root', done => {
+    const Constructor = Vue.extend(App)
+    const router = new VueRouter({
+      routes: myRoutes
     })
-    it('should render correct title to game', done => {
-        const Constructor = Vue.extend(App)
-        const router = new VueRouter({
-            routes: myRoutes
-        })
-        const vm = new Constructor({ router }).$mount()
-        router.push('/game')
+    const vm = new Constructor({ router }).$mount()
+    router.push('/')
+    Vue.nextTick(() => {
+      expect(vm.$el.querySelector('h1').textContent).to.equal('Color Game')
+      done()
+    })
+  })
+  it('should render correct title to game', done => {
+    const Constructor = Vue.extend(App)
+    const router = new VueRouter({
+      routes: myRoutes
+    })
+    const vm = new Constructor({ router }).$mount()
+    router.push('/game')
 
-        try {
-            Vue.nextTick(() => {
-                expect(vm.$el.querySelectorAll('.game-button').length).to.equal(2)
-                done()
-            })
-        } catch (error) {
-            done()
-        }
+    try {
+      Vue.nextTick(() => {
+        expect(vm.$el.querySelectorAll('.game-button').length).to.equal(2)
+        done()
+      })
+    } catch (error) {
+      done()
+    }
+  })
+  it('should render correct title to botGame', done => {
+    const Constructor = Vue.extend(App)
+    const router = new VueRouter({
+      routes: myRoutes
     })
-    it('should render correct title to botGame', done => {
-        const Constructor = Vue.extend(App)
-        const router = new VueRouter({
-            routes: myRoutes
-        })
-        const vm = new Constructor({ router }).$mount()
-        router.push('/botgame')
-        try {
-            Vue.nextTick(() => {
-                expect(vm.$el.querySelectorAll('input[type=range]').length).to.equal(1)
-                done()
-            })
-        } catch (error) {
-            done()
-        }
+    const vm = new Constructor({ router }).$mount()
+    router.push('/botgame')
+    try {
+      Vue.nextTick(() => {
+        expect(vm.$el.querySelectorAll('input[type=range]').length).to.equal(1)
+        done()
+      })
+    } catch (error) {
+      done()
+    }
+  })
+  it('should render correct title to finishgame', done => {
+    const Constructor = Vue.extend(App)
+    const router = new VueRouter({
+      routes: myRoutes
     })
-    it('should render correct title to finishgame', done => {
-        const Constructor = Vue.extend(App)
-        const router = new VueRouter({
-            routes: myRoutes
-        })
-        const vm = new Constructor({ router }).$mount()
-        router.push('/finishgame')
-        try {
-            Vue.nextTick(() => {
-                expect(vm.$el.querySelector('h1').textContent).to.equal('Finish!')
-                done()
-            })
-        } catch (error) {
-            done()
-        }
-    })
+    const vm = new Constructor({ router }).$mount()
+    router.push('/finishgame')
+    try {
+      Vue.nextTick(() => {
+        expect(vm.$el.querySelector('h1').textContent).to.equal('Finish!')
+        done()
+      })
+    } catch (error) {
+      done()
+    }
+  })
 })
